a270accb2eeab862e8ca7c192ddfb8e0385febba
Stanley Kim <stanlejk@usc.edu>
2018-02-25 19:10:34 -0800

    Finished most of HTMLGenerator, just need to learn how to display BufferedImages

--- src/HTMLGenerator.java
+++ src/HTMLGenerator.java
@@ -1,14 +1,43 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
 
 public class HTMLGenerator {
 
 	private CollageManager collageManager;
 	
+	// FOR TESTING PURPOSES
+	public HTMLGenerator()
+	{
+		this.collageManager = new CollageManager();
+	}
+	
+	// Constructor
 	public HTMLGenerator(CollageManager collageManager)
 	{
 		this.collageManager = collageManager;
 	}
 
+	// Main generator function
 	public String generateHTML(String searchText) {
+		
+		// Instantiate file object from template html file
+		File collageTemplateFile = new File("WebContent/collagetemplate.html");
+		// String to hold encoding of file
+		String encoding = "";
+		try {
+			String htmlString = FileUtils.readFileToString(collageTemplateFile, StandardCharsets.UTF_8);
+			String imgURL = "\"" + "images/exCollage.png" + "\"";
+			htmlString = htmlString.replace("$topicString", searchText);
+			htmlString = htmlString.replace("$imgURL", imgURL);
+			FileUtils.writeStringToFile(new File("WebContent/collageresult.html"), htmlString, StandardCharsets.UTF_8);
+		}
+		catch (IOException ioe) {
+			ioe.printStackTrace();
+			return null;
+		}
 		
 		// dummy return
 		String dummy = "dummy";
@@ -21,6 +50,5 @@ public class HTMLGenerator {
 		String dummy = "dummy";
 		return dummy;
 	}
-	
 	
 }
